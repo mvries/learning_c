168f6edc122b80c8770ddf375d08528312ed284b
mvries <m97devries@gmail.com>
2024-05-11 18:36:47 +0200

    Main function and gitignore.
    Added main function for bubblesort and updated gitignore for emacs
    buffers.

---
+++ sorting_algorithms/bubblesort/bubblesort.c
@@ -0,0 +1,10 @@
+/* This is the main function of the bubble sort algortihm */
+#include <array_utils.h>
+
+int main()
+{
+    int array_length = 5;
+    int* array = make_random_array(array_length);
+
+    return(0);
+}
