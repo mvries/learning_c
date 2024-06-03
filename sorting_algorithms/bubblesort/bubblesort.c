82ab81657031ea8f49a4a822573ba6a1afd60ee3
Martijn <m97devries@gmail.com>
2024-06-03 17:40:25 +0200

    All the code is in one script and rough version is working now, scaling to screen next.

--- sorting_algorithms/bubblesort/bubblesort.c
+++ sorting_algorithms/bubblesort/bubblesort.c
@@ -1,41 +1,128 @@
-/* This is the main function of the bubble sort algortihm */
-#include "bubblesort_utils.h"
-#include <time.h>
+/* This script is a c implementation of the bubblesort algorithm. */
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <SDL2/SDL.h>
 
-int main()
+
+/* Function to generate a randomly scrambled array of continues numbers. */
+int* make_sorting_array(int array_size)
 {
-    // Generate an array
-    int array_length = 100;
-    int* array = make_random_array(array_length);
-    int search_index = array_length - 1;
+  // Make the array to sort and put on the heap:
+  int* sorting_array = malloc(array_size*sizeof(int));
 
-    //Keep track of time
-    clock_t start, end;
-    double cpu_time_used;
-
-    start  = clock();
-    
-    while (search_index > 1)
+  // Populate array with integers:
+  for (int i = 1; i <= array_size; i++)
     {
-        //perform bubblesort on the array
-        for (int i = 0; i < search_index; i++)
-        {
-	    if (compare_items(array, i, i + 1 ) == 1)
-	    {
-		swap_items(array, i, i + 1);
-	    }
-        }
-	search_index -= 1;
+      sorting_array[i - 1] = i; 
     }
 
-    end = clock();
+  // Then we scramble the array:
+  time_t t;
+  srand((unsigned) time(&t));
+  for (int i = 0; i < array_size; i++)
+    {
+      int temp = sorting_array[i];
+      int random_index = rand() % array_size;
 
-    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
-    
-    // Print out the sorted array and runtime
-    print_array(array, array_length);
-    printf("Runtime: %f seconds\n", cpu_time_used);
-    
-    return(0);
+      sorting_array[i] = sorting_array[random_index];
+      sorting_array[random_index] = temp;
+    }
+  
+  return sorting_array;
 }
+
+
+/* Function to prepare a screen for the program. */
+int prepare_screen(SDL_Window** screen, SDL_Renderer** renderer, int window_height, int window_width)
+{
+  
+  if (SDL_Init(SDL_INIT_VIDEO) != 0)
+  {
+    fprintf(stderr, "Could not init SDL: %s\n", SDL_GetError());
+    return 1;
+  }
+
+  *screen = SDL_CreateWindow("Bubblesort", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, window_height, window_width,0);
+  if(!*screen)
+    {
+      fprintf(stderr, "Could not create window\n");
+      return 1;
+    }
+
+  *renderer = SDL_CreateRenderer(*screen, -1, SDL_RENDERER_SOFTWARE);
+  if(!*renderer)
+    {
+      fprintf(stderr, "Could not create renderer\n");
+      return 1;
+    }
+  
+  SDL_SetRenderDrawColor(*renderer, 255, 255, 255, 255);
+  SDL_RenderClear(*renderer);
+  SDL_RenderPresent(*renderer);
+  return 0;
+}
+
+
+/* Function to update the SDL point array based on the sorting array. */
+int update_SDL_array(SDL_Rect* SDL_rect_array, int* sorting_array, int array_size)
+{
+  SDL_Rect new_point;
+
+  for (int i = 0; i < array_size; i++)
+    {
+      new_point.x = i;
+      new_point.y = sorting_array[i];
+      new_point.w = 5;
+      new_point.h = 5;
+      SDL_rect_array[i] = new_point;
+    }
+  return 0;
+}
+
+
+/* Function to draw the points in the SDL point array to the screen: */
+int draw_SDL_array(SDL_Rect* SDL_array, int array_size, SDL_Renderer* renderer)
+{
+  SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); 
+  SDL_RenderDrawRects(renderer, SDL_array, array_size);
+  SDL_RenderPresent(renderer);  
+}
+
+
+/* Main function of the script */
+int main()
+{
+  // Size of the array to be sorted:
+  int array_size = 100;
+
+  // Generate array to be sorted:
+  int* sorting_array = make_sorting_array(array_size);
+  
+  // Prepare the screen:
+  int window_height = 680;
+  int window_width = 480;
+  SDL_Window* screen;
+  SDL_Renderer* renderer;
+  
+  prepare_screen(&screen, &renderer, window_height, window_width);
+
+  // Generate an array in heap to store the SDL points:
+  struct SDL_Rect
+  {
+    int x, y;
+    int w, h;
+  };
+
+  SDL_Rect* SDL_rect_array = (SDL_Rect*)malloc(array_size * sizeof(SDL_Rect));
+
+  // Update the array with the initial values:
+  update_SDL_array(SDL_rect_array, sorting_array, array_size);
+
+  // Draw the initial points to the screen:
+  draw_SDL_array(SDL_rect_array, array_size, renderer);
+
+  SDL_Delay(10000);
+  return 0;
+}
+
